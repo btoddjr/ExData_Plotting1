b845bb6c580a63635484a82327d6296e271178cf
Billy Todd <btoddjr@gmail.com>
2021-02-13 16:46:55 -0600

    R Code for Plot 4

---
+++ plot4.R
@@ -0,0 +1,34 @@
+#read the data file
+
+powerdata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+#subset the data by the date range
+
+powerset <- subset(powerdata, powerdata$Date %in% c("1/2/2007", "2/2/2007"))
+
+#create list of date/time combos from the subset data
+
+combdatetime <- strptime(paste(powerset$Date, powerset$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+
+#open the plot device
+
+png("plot4.png", width = 480, height = 480)
+
+#plot the charts in a 2x2 format
+
+par(mfrow = c(2, 2)) 
+
+plot(combdatetime, as.numeric(powerset$Global_active_power), type = "l", xlab = "", ylab = "Global Active Power")
+
+plot(combdatetime, as.numeric(powerset$Voltage), type = "l", xlab = "datetime", ylab = "Voltage")
+
+plot(combdatetime, as.numeric(powerset$Sub_metering_1), type = "l", xlab = "", ylab = "Energy sub metering")
+lines(combdatetime, as.numeric(powerset$Sub_metering_2), type = "l", col = "red")
+lines(combdatetime, as.numeric(powerset$Sub_metering_3), type = "l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 3, col = c("black", "red", "blue"), bty = "n", cex = .8)
+
+plot(combdatetime, as.numeric(powerset$Global_reactive_power), type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
+#close the plot device
+
+dev.off()
